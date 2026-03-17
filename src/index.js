efbce12082d828e2193f0c1418b7eae597ed80f5
Anucs1304 <151374811+Anucs1304@users.noreply.github.com>
2026-03-17 18:05:24 +0000

    updated index with home page, contact, menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("Webpack is running!");
+//console.log("Webpack is running!");
+import loadHome from "./home";
+import loadMenu from "./menu";
+import loadContact from "./contact";
+
+function clearContent() {
+  document.getElementById("content").innerHTML = "";
+}
+
+function addNavEvents() {
+  document.getElementById("home-btn").addEventListener("click", () => {
+    clearContent();
+    loadHome();
+  });
+
+  document.getElementById("menu-btn").addEventListener("click", () => {
+    clearContent();
+    loadMenu();
+  });
+
+  document.getElementById("contact-btn").addEventListener("click", () => {
+    clearContent();
+    loadContact();
+  });
+}
+
+function init() {
+  addNavEvents();
+  loadHome();
+}
+
+init();
